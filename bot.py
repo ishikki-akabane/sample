149a6353dc81fec9ff98d5b9c42bf528e50b9083
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2024-07-21 21:26:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 import asyncio
 import logging
+import time
 
 
 TOKEN = "7248055214:AAEFfAsUxVqAjmqtrPdjdho2Lmvy3K7qF8Y"
@@ -21,12 +22,24 @@ logging.basicConfig(
 LOGGER = logging.getLogger('[ISHIKKI]')
 LOGGER.info("Test server is booting up...")
 
+StartTime = time.time()
+
 app = Client("sample", API_ID, API_HASH, bot_token=TOKEN)
+
 
 @app.on_message(filters.command("start"))
 async def start_command(client, message):
-    user_id = message.from_user.id    
-    await message.reply_text("hii")
+    user_id = message.from_user.id
+    current_time = time.time()
+    uptime_seconds = current_time - StartTime
+    uptime_hours = int(uptime_seconds // 3600)
+    uptime_minutes = int((uptime_seconds % 3600) // 60)
+    
+    final_txt = f"""Hoiiiii!!
+    
+Uptime: {uptime_hours} hour {uptime_minutes} mins
+"""
+    await message.reply_text(final_txt)
 
 print("started...")
 app.run()
