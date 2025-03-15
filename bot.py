3f9f75281f45fbe873cb253b7b886a48a84036f6
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2025-03-15 16:31:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import logging
 import time
 
 
-TOKEN = "7248055214:AAEFfAsUxVqAjmqtrPdjdho2Lmvy3K7qF8Y"
+TOKEN = "7880857077:AAFU8ELLOCcEU0wwD-6f0aURRraQjNGar_Y"
 API_ID = 25629197  # Your API ID
 API_HASH = "fd41f8bacda97ab0a3ad120b30339978"  # Your API Hash
 
